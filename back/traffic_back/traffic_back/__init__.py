5fa25610895e82853b352c7bfd614a5b93d53861
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2019-03-26 15:02:43 +0100

    run through `flask run`

--- back/traffic_back/traffic_back/__init__.py
+++ back/traffic_back/traffic_back/__init__.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python
 #coding=utf-8
+
+import connexion
+from flask_cors import CORS
+from flask import Flask
+
+def create_app():
+    app = connexion.FlaskApp(__name__, specification_dir='./static')
+    CORS(app.app)
+    app.add_api('swagger.yaml')
+
+    return app.app
