b408dfc3ddf03abf3bcf0c330cc2ec43c276539a
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2019-03-26 13:51:49 +0100

    added project README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+## Traffic analyser app
+
+Frontend (React.JS) and Backend (Flask) components for the traffic analyser. To have a working app, both must be run separately.
---
+++ front/traffic-dashboard/src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { LineChart, Line } from 'recharts';
+
+class TrafficSeries extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            data: []}
+    }
+
+    
+    componentDidMount() {
+        fetch("http://localhost:8080/api/traffic")
+            .then(res => res.json())
+            .then(
+                  (result) => {
+                      this.setState({
+                          isLoaded: true,
+                          data: result.data
+                      });
+                  },
+                  (error) => {
+                      this.setState({
+                          isLoaded: true,
+                          error: error});
+             }
+            )
+    }
+
+    render() { return (
+              <LineChart width={400} height={400} data={this.state.data} className='chart'>
+                <Line type="monotone" dataKey="debit" stroke="#8884d8" />
+              </LineChart>
+    );
+  }
+}
+
+class App extends Component {
+  render() { return <TrafficSeries></TrafficSeries> }
+}
+
+export default App;
