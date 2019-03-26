a304870a63bfd776e579d1e2135dbfdc48039ed7
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2019-03-26 13:48:33 +0100

    add cors to work with frontend

--- app.py
+++ app.py
@@ -2,7 +2,9 @@
 #coding=utf-8
 
 import connexion
+from flask_cors import CORS
 
 app = connexion.FlaskApp(__name__, specification_dir='./')
+CORS(app.app)
 app.add_api('swagger.yaml')
 app.run(port=8080, debug=True)
